f3bee2523efe716d6a7c786cf17cdc76038cd6aa
Bruno <brunobromo321@gmail.com>
2024-08-31 10:51:18 -0300

    feat - configuração do banco

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_DATABASE || 'node',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+    migrationStorage: 'json',
+    migrationStoragePath: path.resolve(__dirname, 'database/migration-meta.json'),
+    seederStorage: 'json',
+    seederStoragePath: path.resolve(__dirname, 'database/seeder-meta.json'),
+    migrationStorageTableName: 'migrations',
+    migrationStorageTableSchema: 'schema',
+  },
+  // test: {
+  //   username: process.env.DB_USERNAME || 'root',
+  //   password: process.env.DB_PASSWORD || null,
+  //   database: process.env.DB_DATABASE || 'database_test',
+  //   host: process.env.DB_HOST || '127.0.0.1',
+  //   dialect: 'mysql'
+  // },
+  // production: {
+  //   username: process.env.DB_USERNAME || 'root',
+  //   password: process.env.DB_PASSWORD || null,
+  //   database: process.env.DB_DATABASE || 'database_production',
+  //   host: process.env.DB_HOST || '127.0.0.1',
+  //   dialect: 'mysql'
+  // }
+};
--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const api = require('./routes/api')
 
 app.use('/api', api);
 
-app.listen(port, () => {
-    console.log("Conexão realizada com sucesso!")
-})
+module.exports = app;
