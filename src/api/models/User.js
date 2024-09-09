80ad909039390cca97114184577e64268eaeef6b
Bruno <brunobromo321@gmail.com>
2024-09-09 14:32:26 -0300

    feat - criação das models

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -1,15 +1,35 @@
 'use strict';
-const {Model} = require('sequelize');
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
+      // Relação one-to-many com PasswordResetToken
       User.hasMany(models.PasswordResetToken, {
         foreignKey: 'user_id',
-        as: 'passwordResetToken',
+        as: 'passwordResetTokens',
+        onUpdate: 'CASCADE'
+      });
+
+      // Relação many-to-many com Role
+      User.belongsToMany(models.Role, {
+        through: 'users_roles', 
+        foreignKey: 'user_id', 
+        otherKey: 'role_id',  
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE'
+      });
+
+      // Relação many-to-many com Permission
+      User.belongsToMany(models.Permission, {
+        through: 'users_permissions', 
+        foreignKey: 'user_id',
+        otherKey: 'permission_id',
+        onUpdate: 'CASCADE'
       });
     }
   }
+
   User.init({
     name: {
       type: DataTypes.STRING,
@@ -22,14 +42,14 @@ module.exports = (sequelize, DataTypes) => {
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
-   
   }, {
     sequelize,
     modelName: 'User',
     tableName: 'users',
     timestamps: true
   });
+
   return User;
 };
---
+++ src/api/models/role.js
@@ -0,0 +1,44 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    static associate(models) {
+      // Relação many-to-many com User
+      Role.belongsToMany(models.User, {
+        through: 'users_roles', // Nome da tabela intermediária
+        foreignKey: 'role_id',  // Chave estrangeira na tabela intermediária
+        otherKey: 'user_id',    // Outra chave estrangeira na tabela intermediária
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE'
+      });
+
+      // Relação many-to-many com Permission
+      Role.belongsToMany(models.Permission, {
+        through: 'roles_permissions', // Nome da tabela intermediária
+        foreignKey: 'role_id',        // Chave estrangeira na tabela intermediária
+        otherKey: 'permission_id',    // Outra chave estrangeira na tabela intermediária
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE'
+      });
+    }
+  }
+
+  Role.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+  }, {
+    sequelize,
+    modelName: 'Role',
+    tableName: 'roles',
+    timestamps: true
+  });
+
+  return Role;
+};
