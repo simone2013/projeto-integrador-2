6033134856a64f0b73e9d9b5a600cd8bb4b5f581
Bruno Oliveira <brunobromo321@gmail.com>
2024-11-09 19:18:51 -0300

    correcao doador cadastro

--- src/http/controllers/Auth/DistributionController.js
+++ src/http/controllers/Auth/DistributionController.js
@@ -30,10 +30,18 @@ const create = async (req, res) => {
       const request = req.body;
       const id = req.body.resource_id;
     
-      const findResource = await Resource.findByPk(id);
-      
-      const quantity = req.body.quantity + findResource.quantity;
-      await Resource.update({ quantity: quantity }, { where: { id: id } });
+      const findResource = await Resource.findOne({
+        where: {
+          name: req.body.name,  // Substitua pelo nome desejado
+        }
+      });
+      console.log(req.body.quantity, findResource.quantity )
+      if(req.body.quantity > findResource.quantity ){
+        res.status(500).json({ error: error.message });
+      }
+     
+      const quantity =findResource.quantity - req.body.quantity ;
+      await Resource.update({ quantity: quantity }, { where: { name: req.body.name } });
       const newUser = await Distribution.create(request);
      
       res.status(201).json(newUser);
--- src/http/controllers/Auth/DonationsController.js
+++ src/http/controllers/Auth/DonationsController.js
@@ -26,13 +26,24 @@ const index = async (req, res) => {
 
 
 const create = async (req, res) => {
-    try {
-      const request = req.body;
-      const newUser = await Donation.create(request);
-      res.status(201).json(newUser);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
+  try {
+    const request = req.body;
+    const id = req.body.resource_id;
+  
+    const findResource = await Resource.findOne({
+      where: {
+        name: req.body.name,  // Substitua pelo nome desejado
+      }
+    });
+   
+    const quantity = req.body.quantity + findResource.quantity;
+    await Resource.update({ quantity: quantity }, { where: { name: req.body.name } });
+    const newUser = await Donation.create(request);
+   
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {
